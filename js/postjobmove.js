8b5da158a0199fef2d1ba263293ffeae420cb0f9
Atheer Azaizeh <96442559+AtheerAzaizeh@users.noreply.github.com>
2024-07-27 20:30:36 +0300

    Delete js directory

--- js/postjobmove.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('back-button').addEventListener('click', function(event) {
-        event.preventDefault();
-        window.location.href = 'desktophomepage.html';
-    });
-});
